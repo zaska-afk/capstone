5ff554cb50adde1b7f4679f99065e4a5fe03fd16
DMoss-dimos <dimoss46@outlook.com>
2021-10-05 16:07:21 -0400

    test commit

--- project/models.py
+++ project/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# first models 
 class Profile(models.Model):
     display_name = models.CharField(max_length=50)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
