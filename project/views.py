65e9c63df3c3a1cc9022ebc842e8a232d84efb3a
zaska-afk <zacharybalaska@gmail.com>
2021-10-07 20:47:25 -0400

    new branch commit

--- project/views.py
+++ project/views.py
@@ -67,38 +67,34 @@ def edit(request, id):
 def home_view(request):
     ...
 # By Dunya-trying create an add_post with @ user ability
-# Dunya - structure somewhat taken from twitterclone(Not sure about lines 84 and 85)
+# structure somewhat taken from twitterclone(Not sure about lines 84 and 85)
 def addpost_view(request):
     if request.method == "POST":
         form = AddPostForm(request.POST)
         if form.is_valid():
-            # form.save()
+            form.save()
             data = form.cleaned_data
             add_post = Post.objects.create(
-                post_content=data["post_name", "post_text", "post_on_comm"],
-                # post_content=data["post_name", "post_text"],
-                post_creator=request.user
+                post_content=data["post_name", "post_text"],
+                post_=request.user
             )
             # if "@" in data["post_text"]:
             #     find_user = re.findall(r"@(\w+)", data["post_content"])
             #     grap_user = find_user[0]
-                # user = post_on_comm.objects.get(username=grap_user)
+            #     # user = post_on_comm.objects.get(username=grap_user)
                 # Notification.objects.create(post_creator=user, add_post=add_post)
             return redirect('/')
 
     form = AddPostForm()
-    return render(request, "generic_form.html", {"form": form})
+    return render(request, "index.html", {"form": form})
 
-# Dunya - took structure some what from recipebox
-# Dunya - not sure about the structure
-# Dunya - Changed view a bit to help render. commented out olde to have a reference
+# took structure some what from recipebox
+# not sure about the structure
 def addcomment_view(request):
-    template_name = "generic_form.html"
-    # comment = Post.objects.get(id=id) # Dunya - Not sure about use of id
-    comment = Post.objects.create("on_post, comment_text")
-    # context = {"comment": comment}
-    # return render(request, template_name, context)
-    return render(request, template_name)
+    template_name = "index.html"
+    comment = Post.objects.get(id=id) # Not sure about use of id
+    context = {"comment": comment}
+    return render(request, template_name, context)
 
 def addcommunity_view(request):
     ...
