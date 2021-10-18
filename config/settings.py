cc59a04be311745f76187cfc8e34ea99278171d2
zaska-afk <zacharybalaska@gmail.com>
2021-10-18 10:35:29 -0400

    secret key added

--- config/settings.py
+++ config/settings.py
@@ -11,17 +11,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 import os
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zf*(is+&vp74to@#@ab_v=oe+!!4h%i7ia1_4dw%dcxr!002%1'
+SECRET_KEY = env('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
